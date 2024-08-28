9ca58d0f10e659cd6120eca6175aa116ed3b785c
Сергей Сушков <sushkov_sv@ursip.ru>
2024-08-28 10:41:18 +0300

    add docker compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,10 @@
-uvicorn api.app:app --host 0.0.0.0 --port 8088
+#!/bin/bash
+
+# Экспортируем переменные из .env файла
+export $(grep -v '^#' .env | xargs)
+
+# Выполняем миграции Alembic
+alembic upgrade head
+
+# Запускаем приложение
+exec uvicorn api.app:app --host 0.0.0.0 --port ${PORT}
